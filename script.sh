35697eb63f568aa87b8703b577ce7ddd0eaa1b0b
Juhi Ghosh <juhighosh04@gmail.com>
2020-07-30 00:24:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,6 @@ echo "juhi"
 sudo mkdir -p /tmp/my-data
 sudo echo "some dummy content and some more dummy content and some more and more" > /tmp/my-data/myfile.txt
 sudo docker pull busybox
+sudo docker build -t sample-busy-image <folder-name-having-Dockerfile
 sudo docker run -it -v /tmp/my-data:/tmp/my-data --name my-container busy-image
 echo "ghosh"
